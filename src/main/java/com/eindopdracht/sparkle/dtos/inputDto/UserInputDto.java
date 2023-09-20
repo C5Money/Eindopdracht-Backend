b4b0feeb612826b03cf7beb30ee1f94ea8d6b89a
C5Money <misriffi@gmail.com>
2023-09-20 14:42:07 +0200

    Final version

--- src/main/java/com/eindopdracht/sparkle/dtos/inputDto/UserInputDto.java
+++ src/main/java/com/eindopdracht/sparkle/dtos/inputDto/UserInputDto.java
@@ -68,34 +68,3 @@ public class UserInputDto {
 }
 
 
-//    @NotBlank(message = "Username is required")
-//    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long")
-//    public String userName;
-//
-//    @NotBlank
-//    @Size(min=6, max=30)
-//    public String password;
-//
-//    @NotBlank(message = "Email is required")
-//    @Email
-//    public String email;
-//
-//    @NotBlank(message = "First name is required")
-//    @Size(max = 50, message = "First name cannot exceed 50 characters")
-//    public String firstName;
-//
-//    @NotBlank(message = "Last name is required")
-//    @Size(max = 50, message = "Last name cannot exceed 50 characters")
-//    public String lastName;
-//
-//    @NotBlank(message = "Address is required")
-//    @Size(max = 100, message = "Address cannot exceed 100 characters")
-//    public String address;
-//
-//    @NotBlank(message = "Zip code is required")
-//    public String zipCode;
-//
-//    @NotBlank(message = "Phone number is required")
-//    @Pattern(regexp = "\\d{10}", message = "Phone number must be a 10-digit number")
-//    public String phoneNumber;
-
